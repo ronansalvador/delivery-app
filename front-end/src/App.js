ef63ca4466a8960a09648db77ca8f63dcc104568
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-01-20 17:38:00 -0300

    feat(routes): criando rotas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+// import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={ <Home /> } /> */}
+        <Route exact path="/" element={ <Navigate to="/login" /> } />
+        <Route exact path="/login" element={ <Login /> } />
+        <Route exact path="/register" element={ <Register /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-// iniciando
