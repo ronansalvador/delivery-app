cfab33df294c8f92c67307e99da20a40d1fc92a0
Guilherme Palma <guilhermepallma@gmail.com>
2023-01-24 14:30:46 -0400

    fix: update branch

--- back-end/src/utils/decyptMD5.js
+++ back-end/src/utils/decyptMD5.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 // Função que desencripta senha do banco de dados.
 const compareHash = (password, originalHash) => {
   const newHash = crypto.createHash('md5').update(password).digest('hex');
+  // Retorna true ou false.
   return newHash === originalHash;
 };
 module.exports = { compareHash };
