4d3b2f004f1b96952898b3574deaaaa4a6288486
Marcelo Carvalho <marcelo.carvalho2@outlook.com>
2023-01-24 14:57:29 -0300

    jwt

--- back-end/src/routes/login.routes.js
+++ back-end/src/routes/login.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const loginController = require('../controllers/login.controller');
 
 const loginRoutes = express.Router();
-
+//
 loginRoutes.post('/', loginController.validateLogin);
 
 module.exports = loginRoutes; 
