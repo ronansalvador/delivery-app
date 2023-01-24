cedb3d175e67cefd2c183faf0adbca0b793eb5a6
celso-rodrigo <celsorodrigoma@gmail.com>
2023-01-24 13:37:47 -0300

    feat: Criada base da tela de login com seus respectivos data-testid.

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,6 +2,58 @@ import React from 'react';
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div>
+      <h1>Cadastro</h1>
+      <form>
+        <label htmlFor="register_name">
+          <input
+            type="text"
+            data-testid="common_register__input-name"
+            id="register_name"
+            placeholder="Seu nome"
+            // value={}
+            // onChange={}
+          />
+        </label>
+
+        <label htmlFor="register_email">
+          <input
+            type="text"
+            data-testid="common_register__input-email"
+            id="register_email"
+            placeholder="seu-email@site.com"
+            // value={}
+            // onChange={}
+          />
+        </label>
+
+        <label htmlFor="register_password">
+          <input
+            type="password"
+            data-testid="common_register__input-password"
+            id="register_password"
+            placeholder="********"
+            // value={}
+            // onChange={}
+          />
+        </label>
+
+        <button
+          type="button"
+          // disabled={}
+          data-testid="common_register__button-register"
+          // onClick={}
+        >
+          CADASTRAR
+        </button>
+
+        <p
+          className={ `register-error ${!('message' in {}) && 'hidden'}` }
+          data-testid="common_register__element-invalid_register"
+        >
+          {}
+        </p>
+      </form>
+    </div>
   );
 }
