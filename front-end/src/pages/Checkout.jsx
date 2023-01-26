9b338fbd0bc63f4d3ee7938fcd3f02d70fc73376
celso-rodrigo <celsorodrigoma@gmail.com>
2023-01-26 14:14:24 -0300

    docs: Comentadas linhas de código em progresso

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import CheckoutItem from '../components/CheckoutItem';
 import Navbar from '../components/Navbar';
 import CartContext from '../context/CartContext';
