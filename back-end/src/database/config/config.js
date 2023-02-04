5875eafa0e41037697e24d2c538cd431bab804fe
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-02-04 08:56:51 -0300

    feat(deploy): configuração de variaveis de ambiente para deploy

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -11,12 +11,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQL_PORT || process.env.MYSQLPORT || '3306',
+  database: process.env.MYSQL_DB_NAME || process.env.MYSQLDBNAME,
+    // `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQL_USER || process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
