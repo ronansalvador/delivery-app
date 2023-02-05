391c768ec4225f0875cd88a5adc6ac947dd05ee4
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-02-04 21:14:45 -0300

    feat(order sales): alterando renderização quando não ha pedidos

--- front-end/src/pages/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders.jsx
@@ -15,25 +15,30 @@ export default function CustomerOrders() {
     loadSales();
   }, [sales]);
 
+  const showOrders = () => {
+    if (sales.length === 0) return <h3>Cliente sem pedidos</h3>;
+    return (loading
+      ? <div className="loading" />
+      : (
+        <>
+          { sales.map((order) => (
+            <div className="order-card-container" key={ order.id }>
+              <OrderCard
+                orderDetails={ order }
+                currentRole="customer"
+              />
+            </div>
+          )) }
+        </>
+
+      ));
+  };
+
   return (
     <div>
       <Navbar />
       <div className="page-order-card">
-        {loading
-          ? <div className="loading" />
-          : (
-            <>
-              { sales.map((order) => (
-                <div className="order-card-container" key={ order.id }>
-                  <OrderCard
-                    orderDetails={ order }
-                    currentRole="customer"
-                  />
-                </div>
-              )) }
-            </>
-
-          )}
+        {showOrders()}
       </div>
     </div>
   );
